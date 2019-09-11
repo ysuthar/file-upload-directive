137cb6e103183c025d624bb7c81f3067b9844584
ysuthar <ysuthar.web@gmail.com>
2019-09-11 13:56:51 +0530

    2 files modified, 1 added, 1 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FileUplaodDirectiveDirective } from './file-uplaod-directive.directive'
-import { FileUplaodInputDirective } from './file-uplaod-directive.directive'
+import { NumberSpinnerDirective } from './number-spinner.directive'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, FileUplaodDirectiveDirective, FileUplaodInputDirective ],
+  declarations: [ AppComponent, NumberSpinnerDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
