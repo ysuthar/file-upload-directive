49dfc24cb79eb55edf6ee77cf4fc7e2560f9ccc7
ysuthar <ysuthar.web@gmail.com>
2019-09-18 19:00:10 +0530

    5 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FileUploadDirectiveDirective } from './file-upload-directive'
+import { FileUploadDirective } from './file-upload-directive'
 import { FileUploadInputDirective } from './file-upload-directive'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, FileUploadDirectiveDirective, FileUploadInputDirective ],
+  declarations: [ AppComponent, FileUploadDirective, FileUploadInputDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- app/file-upload-directive.ts
+++ app/file-upload-directive.ts
@@ -5,7 +5,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Directive({
   selector: '[FileUplaod]'
 })
-export class  FileUploadDirectiveDirective {
+export class  FileUploadDirective {
   url = '';
   file = ''
   img;
@@ -24,17 +24,32 @@ export class  FileUploadDirectiveDirective {
       this.elRef.nativeElement.getElementsByClassName('FileUpload').item(0).value = '';
     }
 
-    if(event.target.classList.contains('viewImage')){
+if(event.target.classList.contains('btn-close')){
+      this.elRef.nativeElement.getElementsByClassName('modelPopup').item(0).style.display='none'
+    }
 
-    var html = '<html>' +
-      '<style>html, body { padding: 0; margin: 0; } iframe { width: 100%; height: 100%; border: 0;}  </style>' +
-      '<body>' +
-      '<iframe src="' + this.elRef.nativeElement.getElementsByTagName('img').item(0).src + '"></iframe>' +
-      '</body></html>';
-     //window.open("about:blank");
-     this.a = window.open("about:blank", "Big Image");
-     this.a.document.write(html);
-     this.a.document.close();
+    if(event.target.classList.contains('viewImage')){
+      this.elRef.nativeElement.getElementsByClassName('modelPopup').item(0).style.display='block';
+
+
+    // var html = '<html>' +
+    //   '<style>html, body { padding: 0; margin: 0; } iframe { width: 100%; height: 100%; border: 0;}  </style>' +
+    //   '<body>' +
+    //   '<iframe src="' + this.elRef.nativeElement.getElementsByTagName('img').item(0).src + '"></iframe>' +
+    //   '</body></html>';
+
+    // var html = '<div class="modal-header"><button type="button" class="close pull-right" aria-label="Close" (click)="modalRef.hide()"><i class="fa fa-times-circle"></i></button></div><div class="modal-body document-popup"><h6>'+
+    // this.elRef.nativeElement.getElementsByClassName('img-name').item(0).innerHTML +
+    // '</h6><figure><img src="' + 
+    // this.elRef.nativeElement.getElementsByTagName('img').item(0).src + 
+    // '" class="card-img blur-img" alt="Driving Licence"></figure></div>'
+
+    //  //window.open("about:blank");
+    //  //this.a = window.open("about:blank", "Big Image");
+    //  //this.a.document.write(html);
+    // // this.a.document.close();
+    //  var a = document.createElement("<ng-template #template>");
+    //  document.body.innerHTML + a;
     }
   }
 
@@ -44,14 +59,15 @@ export class  FileUploadDirectiveDirective {
 
 export class FileUploadInputDirective {
 
-  @ContentChildren(FileUploadDirectiveDirective) uploaded;
+  @ContentChildren(FileUploadDirective) uploaded;
 
-  url;
+  url = '';
   NumberElement;
   numberValue;
   img;
   reader; 
-  strsubstring
+  strsubstring;
+  result;
 
   constructor(private elRef: ElementRef) {}
 
@@ -67,31 +83,39 @@ export class FileUploadInputDirective {
       reader.readAsDataURL(event.target.files[0]); 
       reader.onload = (event) => { 
       var img = new Image;
-      this.url = reader.result;
-      // **//
-
-
 
       this.strsubstring = FileName.substring(FileName.lastIndexOf("."), FileName.length);
                         if (this.strsubstring == '.jpg' || this.strsubstring == '.png' || this.strsubstring == '.jpeg' || this.strsubstring == '.gif')
                         {
                           this.elRef.nativeElement.parentElement.getElementsByTagName('img').item(0).src = reader.result;
                           this.elRef.nativeElement.parentElement.getElementsByClassName('viewImage').item(0).href = reader.result;
-                          this.elRef.nativeElement.parentElement.getElementsByClassName('img-size').item(0).innerHTML = (Filesize + 'KB');
+                          this.elRef.nativeElement.parentElement.getElementsByClassName('img-size').item(0).innerHTML = (Filesize + 'kb');
                           this.elRef.nativeElement.parentElement.getElementsByClassName('img-name').item(0).innerHTML = FileName;
                           this.elRef.nativeElement.parentElement.getElementsByClassName('FileSuccess').item(0).style.display='block';
                           this.elRef.nativeElement.style.display='none';
+
+
+                          var HTMLTemplate = '<bs-modal-backdrop class="modal-backdrop fade in show"></bs-modal-backdrop><modal-container class="modal fade show" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="btn-close pull-right" aria-label="Close">Remove</button></div><div class="modal-body document-popup"><h6>'+
+                          FileName +
+                          '</h6><figure><img src="' + 
+                          reader.result + 
+                          '" class="card-img blur-img" alt="Driving Licence"></figure></div></div></div></modal-container>'
+                      
+                          this.elRef.nativeElement.parentElement.getElementsByClassName('modelPopup').item(0).insertAdjacentHTML('beforeend', HTMLTemplate) ;
+                          //document.body.innerHTML + a;
+
                         }
                         else {
                           alert('Upload only Valid Format - *.jpg, *.jpeg, *.png, *.gif');
                           this.elRef.nativeElement.value = '';
                         }
-       // **//
-
 
       }
     }
 
+    
+
   }
 
 }
+
