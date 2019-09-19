33d34e8b757c95e4ad3c5eb527ace9fc6ab2a8df
ysuthar <ysuthar.web@gmail.com>
2019-09-19 11:57:36 +0530

    1 file modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,23 +7,5 @@ import { Component , ViewEncapsulation} from '@angular/core';
 encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent  {
-  name = 'Angular 4';
-  url = '';
-  uploaded = false;
 
-  onSelectFile(event) {
-    if (event.target.files && event.target.files[0]) {
-      var reader = new FileReader();
-      var FileURL = event.target.result;
-
-      reader.readAsDataURL(event.target.files[0]); 
-      reader.onload = (event) => { 
-      var img = new Image;
-        this.url = FileURL;
-        this.uploaded = true;
-        img.src = FileURL;
-        console.log(img.width);
-      }
-    }
-  }
 }
